767ebc982eba8b08e7ab5ae5164c320f259e2b07
Eleven <nieshiyi20060927@qq.com>
2026-07-22 10:10:03 +0800

    分别用Java和C语言实现一道关于 [栈] 的经典例题

---
+++ Java/Practice01_Stack.java
@@ -0,0 +1,16 @@
+package Java;
+//Java直接用Deque当栈用
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Practice01_Stack {
+    public static void main(String[] args){
+        String test1 = "({[]})"; 
+        String test2 = "([)]";
+        String test3 = "(()";
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
